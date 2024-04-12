2f2b6bb0905cd8ff85febc5f483247ffc6b385ad
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2024-04-12 11:14:42 -0400

    First commit

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace MauiApp1
+{
+	public partial class App : Application
+	{
+		public App(NavigatorService navigatorService)
+		{
+			InitializeComponent();
+			NavigatorService = navigatorService;
+		}
+
+		public NavigatorService NavigatorService { get; }
+
+		private void MenuItem_Clicked(object sender, EventArgs e)
+		{
+
+			var menuItem = (MenuItem)sender;
+			var url = menuItem.Text switch
+			{
+				"Counter" => "/counter",
+				"Weather" => "/weather",
+				_ => "/"
+			};
+
+			NavigatorService.NavigationManager.NavigateTo(url);
+
+		}
+	}
+}
